359590d2ab294b3a4d6b0def6ab62864ef2867fe
Lillian Layne <144570372+lillianlayne@users.noreply.github.com>
2024-04-19 12:49:36 -0400

    models and home view setup

--- appetitApp/models.py
+++ appetitApp/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
-# Create your models here.
+MEASUREMENTS = (
+  ('c', 'cups'), 
+  ('tbsp', 'tablespoon'), 
+  ('tsp', 'teaspoon'), 
+  ('oz', 'ounces'), 
+  ('fl.oz', 'fluid ounces'), 
+  ('lbs', 'pounds'), 
+)
+
+# Recipe main entity
+class Recipe(models.Model):
+  name = models.CharField(max_length=100)
+  description = models.CharField(max_length=300)
+
+  def __str__(self):
+    return f'{self.name}({self.id})'
+
+
+# Directions model = foreign key for recipe
+class directions(models.Model):
+  description = models.CharField(max_length=400)
+
+  recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return f'{self.description}'
+
+
+# Ingredients model = foreign key for recipe
+class Ingredients(models.Model):
+  ingredient = models.CharField(max_length=150)
+  measurement = models.CharField(
+    max_length=100, 
+    choices=MEASUREMENTS,
+    default=MEASUREMENTS[0][0]
+  )
+  amount = models.IntegerField()
+
+  recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return f'{self.ingredient}'
+  
+
