f835b3bdfa4051c46fa3d248417a927b9137c5cf
Lillian Layne <144570372+lillianlayne@users.noreply.github.com>
2024-04-23 12:04:02 -0400

    style: recipe view styling

--- appetitApp/static/js/recipeFormControls.js
+++ appetitApp/static/js/recipeFormControls.js
@@ -7,6 +7,7 @@ const stepsForm = document.getElementById('add-steps');
 const stepsInput = document.getElementById('id_instructions');
 const addStepsBtn = document.getElementById('add-step-btn');
 const reviewInput = document.getElementById('id_text');
+const doneIngredient = document.getElementById('done-ingredient');
 
 const addStyles = (div, type) => {
   div.classList.add(type)
@@ -26,23 +27,32 @@ for (let div of ingFormDivs) {
 
 addStyles(stepsInput, 'w-full')
 addStyles(reviewInput, 'w-full')
-addStyles(ingredientInput, 'form-input');
-addStyles(ingredientInput, 'w-full');
+addStyles(ingredientInput, 'bg-card-bg')
+addStyles(ingredientInput, 'border-0')
+addStyles(ingredientInput, 'rounded-md')
+addStyles(ingredientInput, 'w-1/2')
+addStyles(ingredientInput, 'm-0')
 addStyles(stepsInput, 'bg-card-bg')
 addStyles(reviewInput, 'bg-card-bg')
 
 
-// window.onload = () => {
-//   ingredientForm.style.display = 'none'
-//   stepsForm.style.display = 'none'
-  
-//   addIngredientBtn.addEventListener('click', (e) => {
-//     ingredientForm.style.display = 'flex';
-//   })
+window.onload = () => {
+ 
+  doneIngredient.addEventListener('click', (e) => {
+    ingredientForm.style.display = 'none';
+    addIngredientBtn.style.display = "inline-flex";
+    doneIngredient.style.display = 'none'
+  })
+  addIngredientBtn.addEventListener('click', (e) => {
+    ingredientForm.style.display = 'flex';
+    doneIngredient.style.display = "inline-flex";
+    addIngredientBtn.style.display = 'none'
+  })
 
-//   addStepsBtn.addEventListener('click', (e) => {
-//     stepsForm.style.display = 'flex'
-//   })
+  addStepsBtn.addEventListener('click', (e) => {
+    stepsForm.style.display = 'block'
+    addStepsBtn.style.display = 'none'
+  })
   
   
-// }
+}
