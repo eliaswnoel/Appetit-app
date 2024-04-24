04e84a5df5b7f50a3b8684e6a1bc71407faa7ba6
Lillian Layne <144570372+lillianlayne@users.noreply.github.com>
2024-04-24 18:22:43 -0400

    deploy

--- appetitApp/models.py
+++ appetitApp/models.py
@@ -69,22 +69,6 @@ class Folder(models.Model):
     return self.name
 
 
-
-class ReviewModel(models.Model):
-  text = models.TextField(max_length=300)
-  recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
-
-  def __str__(self):
-    return f"{self.text}" 
-
-class ReviewModel(models.Model):
-  text = models.TextField(max_length=300)
-  recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
-
-  def __str__(self):
-    return f"{self.text}" 
-  
-
 class Photo(models.Model):
     url = models.CharField(max_length=200)
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
