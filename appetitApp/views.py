c7282b79ae35c4f8cf9e93b92d3f00a712a9e9d9
Lillian Layne <144570372+lillianlayne@users.noreply.github.com>
2024-04-25 09:03:16 -0400

    search

--- appetitApp/views.py
+++ appetitApp/views.py
@@ -18,7 +18,7 @@ recipe_list = '/recipes/list'
 tags_list = '/tags/list'
 get_recipe = '/recipes/get-more-info'
 
-# homepage view
+# startup --------------------
 def home(request): 
   if not request.user.is_authenticated:
     return redirect(f'{settings.LOGIN_URL}?next={request.path}')
@@ -40,21 +40,6 @@ def home(request):
     'tags': tags_display
   })
 
-
-# 13 Search functionality 
-def search_recipes(request):
-  recipe = request.POST['recipe']
-  if request.method == "POST":
-    params = {
-      'from': '0',
-      'size': 20,
-      'q': recipe
-    }
-    recipe_api = accessAPI(recipe_list, params, "GET")
-    recipe_json = recipe_api['results']
-  return render(request, 'search.html', {'recipes': recipe_json, 'recipe_name': recipe})
-
-# 1 view all recipes that a user searches for
 def recipes_index(request, category_name):
   params ={
     'from': '0',
@@ -70,7 +55,20 @@ def recipes_index(request, category_name):
     'folders': folders
   })
 
-# 2 direct to a user created recipe
+def search_recipes(request):
+  recipe = request.POST['recipe']
+  if request.method == "POST":
+    params = {
+      'from': '0',
+      'size': 20,
+      'q': recipe
+    }
+    recipe_api = accessAPI(recipe_list, params, "GET")
+    recipe_json = recipe_api['results']
+  return render(request, 'search.html', {'recipes': recipe_json, 'recipe_name': recipe})
+
+
+# recipe views for user based recipes
 def recipes_user_recipe(request, recipe_id):
   recipe = Recipe.objects.get(id=recipe_id)
   author = recipe.user
@@ -88,7 +86,6 @@ def recipes_user_recipe(request, recipe_id):
     'author': author
   })
 
-# 3 add review to recipe
 def add_review(request, recipe_id):
   form = ReviewForm(request.POST)
 
@@ -99,11 +96,9 @@ def add_review(request, recipe_id):
     new_review.save()
   return redirect('user_recipe', recipe_id=recipe_id)
 
-# delete review from recipe
 class ReviewDelete(DeleteView):
   model = Review
   success_url = '/recipes/user/{recipe_id}/'
-
 
   def delete(self, request, *args, **kwargs):
     self.object = self.get_object()
@@ -111,7 +106,6 @@ class ReviewDelete(DeleteView):
     return redirect(self.get_success_url())
 
 
-# 4 add ingredients to recipe
 def add_ingredient(request, recipe_id):
   form = IngredientForm(request.POST)
 
@@ -121,7 +115,6 @@ def add_ingredient(request, recipe_id):
     new_ingredient.save()
   return redirect('user_recipe', recipe_id=recipe_id)
 
-# 5 add steps
 def add_steps(request, recipe_id):
   form = StepsForm(request.POST)
 
@@ -131,7 +124,6 @@ def add_steps(request, recipe_id):
     new_step.save()
   return redirect('user_recipe', recipe_id=recipe_id)
 
-# 6 direct to an api recipe
 def recipes_detail(request, recipe_id):
   review_form = ReviewForm
   print(recipe_id)
@@ -141,6 +133,38 @@ def recipes_detail(request, recipe_id):
     'recipe': api_recipe,
     'review_form': review_form
   })
+
+
+# CRUD recipes
+class RecipeCreate(CreateView):
+  model = Recipe
+  fields = ['name', 'description']
+
+  def form_valid(self, form):
+      self.object = form.save()
+      return redirect('/recipes')
+
+class RecipeUpdate(UpdateView):
+  model = Recipe
+  fields = ['name', 'description' ]
+
+  def get_absolute_url(self):
+    return self.object.get_absolute_url()
+
+class RecipeDelete(DeleteView):
+   model = Recipe
+   sucess_url = '/home'
+
+   def delete(self, request, *args, **kwargs):
+    self.object = self.get_object()
+    self.object.delete()
+    return redirect('home')
+
+class ReviewUpdate(UpdateView):
+    model = Review
+    form_class = ReviewForm
+    template_name = 'appetitApp/edit_reviews.html' 
+    success_url = '/recipes/user/{recipe_id}/' 
 
 def add_review_api(request, recipe_id):
 
@@ -166,61 +190,26 @@ def add_review_api(request, recipe_id):
     new_review.save()
   return redirect('user_recipe', recipe_id=recipe.id)
 
-# 7 authentication
 def signup(request):
   error_message = ''
   if request.method == 'POST':
-    # This is how to create a 'user' form object
-    # that includes the data from the browser
     form = UserCreationForm(request.POST)
     if form.is_valid():
-      # This will add the user to the database
       user = form.save()
-      # This is how we log a user in via code
       login(request, user)
       return redirect('home.html')
     else:
       error_message = 'Invalid sign up - try again'
-  # A bad POST or a GET request, so render signup.html with an empty form
   form = UserCreationForm()
   context = {'form': form, 'error_message': error_message}
   return render(request, 'registration/signup.html', context)
 
-# 8 user creates a recipe
-class RecipeCreate(CreateView):
-  model = Recipe
-  fields = ['name', 'description']
 
-  def form_valid(self, form):
-      self.object = form.save()
-      return redirect('/recipes')
-
-# 9 user updates a recipe
-class RecipeUpdate(UpdateView):
-  model = Recipe
-  fields = ['name', 'description' ]
-
-  def get_absolute_url(self):
-    return self.object.get_absolute_url()
-
-# 10 user deletes a recipe
-class RecipeDelete(DeleteView):
-   model = Recipe
-   sucess_url = '/home'
-
-# 11 user views all the folders they have started
-class FolderList(ListView):
-  model = Folder
-
-# 12 user creates a folder
+# CRUD Folders
 class FolderCreate(CreateView):
   model = Folder
   fields = '__all__'
   success_url = '/folders/'
-
-def assoc_folder(request, recipe_id, folder_id):
-  Recipe.objects.get(id=recipe_id).folder.add(folder_id)
-  return redirect('user_recipe', recipe_id=recipe_id)
 
 class FolderList(ListView):
   model = Folder
@@ -237,33 +226,25 @@ class FolderDelete(DeleteView):
 
   def delete(self, request, *args, **kwargs):
     self.object = self.get_object()
-    # Add any additional deletion logic here if needed
     self.object.delete()
     return redirect(self.get_success_url())
+
+def assoc_folder(request, recipe_id, folder_id):
+  Recipe.objects.get(id=recipe_id).folder.add(folder_id)
+  return redirect('user_recipe', recipe_id=recipe_id)
 
 def folders_detail(request, folder_id):
   folder = Folder.objects.get(id=folder_id)
   return render(request, 'appetitApp/folder_detail.html', { 'folder': folder })
 
-# edit review
-class ReviewUpdate(UpdateView):
-    model = Review
-    form_class = ReviewForm
-    template_name = 'appetitApp/edit_reviews.html'  # Your edit review template
-    success_url = '/recipes/user/{recipe_id}/' 
-
-
 def add_photo(request, recipe_id):
   photo_file = request.FILES.get('photo-file', None)
   if photo_file:
       s3 = boto3.client('s3')
-      # need a unique "key" for S3 / needs image file extension too
       key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
-      # just in case something goes wrong
       try:
           bucket = os.environ['S3_BUCKET']
           s3.upload_fileobj(photo_file, bucket, key)
-          # build the full url string
           url = f"{os.environ['S3_BASE_URL']}{bucket}/{key}"
           Photo.objects.create(url=url, recipe_id=recipe_id)
       except Exception as e:
